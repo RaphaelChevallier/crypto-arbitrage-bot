2388cfdd2fd728e300d1e31500ef5fdea678433b
Raphael Chevallier <raphaelchevallier@hotmail.com>
2021-01-26 00:23:22 -0800

    final changes

--- index.js
+++ index.js
@@ -76,29 +76,30 @@ async function waitTransaction(txHash) {
     return (tx.status);
 }
 
-function approveDaiToken(tokenInstance, receiver, amount, estimatedGasPrice, callback) {
-    tokenInstance.methods.approve(receiver, amount).send({ from: fromWallet, gas: gasAmount, gasPrice: estimatedGasPrice }, async function(error, txHash) {
-        if (error) {
-            console.log("Dai could not be approved", error);
-            return;
-        }
-        console.log("Dai token approved to " + receiver);
-        const status = await waitTransaction(txHash);
-        if (!status) {
-            console.log("Approval transaction failed.");
-            return;
-        }
-        callback();
-    })
-}
+// function approveDaiToken(tokenInstance, receiver, amount, estimatedGasPrice, callback) {
+//     tokenInstance.methods.approve(receiver, amount).send({ from: fromWallet, gas: gasAmount, gasPrice: estimatedGasPrice }, async function(error, txHash) {
+//         if (error) {
+//             console.log("Dai could not be approved", error);
+//             return;
+//         }
+//         console.log("Dai token approved to " + receiver);
+//         const status = await waitTransaction(txHash);
+//         if (!status) {
+//             console.log("Approval transaction failed.");
+//             return;
+//         }
+//         callback();
+//     })
+// }
 
-async function getQuotes(ethAddress, daiAddress, amount, callback) {
+async function getQuotesEthToDai(ethAddress, daiAddress, amount, callback) {
     let quoteEthToDai = null;
     let quoteDaiToEth = null;
     let tradePossible = false;
+    console.log("Quotes Eth to Dai Trades")
     try {
-        quoteEthToDai = await onesplitContract.methods.getExpectedReturn(ethAddress, daiAddress, amount, 10, 0).call();
-        quoteDaiToEth = await onesplitContract.methods.getExpectedReturn(daiAddress, ethAddress, new BigNumber(new BigNumber(quoteEthToDai.returnAmount).shiftedBy(-fromTokenDecimals)).shiftedBy(fromTokenDecimals).toFixed(), 10, 0).call();
+        quoteEthToDai = await onesplitContract.methods.getExpectedReturn(ethAddress, daiAddress, amount, 1, 0x40000000).call();
+        quoteDaiToEth = await onesplitContract.methods.getExpectedReturn(daiAddress, ethAddress, new BigNumber(new BigNumber(quoteEthToDai.returnAmount).shiftedBy(-fromTokenDecimals)).shiftedBy(fromTokenDecimals).toFixed(), 1, 0x40000000).call();
     } catch (error) {
         console.log('Impossible to get the quote', error)
     }
@@ -107,7 +108,7 @@ async function getQuotes(ethAddress, daiAddress, amount, callback) {
     console.log("Trade Amount: " + new BigNumber(amount).shiftedBy(-fromTokenDecimals).toString() + " Ether");
     console.log("Dai Expected Return: " + new BigNumber(quoteEthToDai.returnAmount).shiftedBy(-fromTokenDecimals).toString());
     console.log("Using Dexes:");
-    for (let index = 0; index < quoteEthToDai.distribution.length; index++) {
+    for (let index = 0; index < oneSplitDexes.length; index++) {
         console.log(oneSplitDexes[index] + ": " + quoteEthToDai.distribution[index] + "%");
     }
     console.log("Trade From: " + daiAddress);
@@ -115,76 +116,97 @@ async function getQuotes(ethAddress, daiAddress, amount, callback) {
     console.log("Trade Amount: " + new BigNumber(quoteEthToDai.returnAmount).shiftedBy(-fromTokenDecimals) + " Dai");
     console.log("Eth Expected Return: " + new BigNumber(quoteDaiToEth.returnAmount).shiftedBy(-fromTokenDecimals).toString())
     console.log("Using Dexes:");
-    for (let index = 0; index < quoteDaiToEth.distribution.length; index++) {
+    for (let index = 0; index < oneSplitDexes.length; index++) {
         console.log(oneSplitDexes[index] + ": " + quoteDaiToEth.distribution[index] + "%");
     }
     console.log(new BigNumber(quoteDaiToEth.returnAmount).shiftedBy(-fromTokenDecimals).toString() - new BigNumber(amount).shiftedBy(-fromTokenDecimals).toString())
-    if(new BigNumber(quoteDaiToEth.returnAmount).shiftedBy(-fromTokenDecimals).toString() - new BigNumber(amount).shiftedBy(-fromTokenDecimals).toString() > .0015){
+    if(new BigNumber(quoteDaiToEth.returnAmount).shiftedBy(-fromTokenDecimals).toString() - new BigNumber(amount).shiftedBy(-fromTokenDecimals).toString() > .015){
         numberOfExchanges = numberOfExchanges + 1;
         tradePossible = true;
-        callback(quoteEthToDai, quoteDaiToEth, tradePossible);
+        return [quoteEthToDai, quoteDaiToEth, tradePossible];
       }
+    if(new BigNumber(quoteDaiToEth.returnAmount).shiftedBy(-fromTokenDecimals).toString() - new BigNumber(amount).shiftedBy(-fromTokenDecimals).toString() > maxDiff){
+        maxDiff = new BigNumber(quoteDaiToEth.returnAmount).shiftedBy(-fromTokenDecimals).toString() - new BigNumber(amount).shiftedBy(-fromTokenDecimals).toString()
+    }
+    console.log("Max Diff so Far: " + maxDiff)
     console.log("Number of Current Exchanges that Happened: " + numberOfExchanges)
-    
+    return [quoteEthToDai, quoteDaiToEth, tradePossible];
 }
 
-async function Trading(daiAddress, ethAddress, amountWithDecimalsEth, estimatedGasPrice){
-    await getQuotes(ethAddress, daiAddress, amountWithDecimalsEth, async function(quoteEthToDai, quoteDaiToEth, tradePossible) {
-        if(tradePossible){
-            let status = false;
-            console.log("/n Swapping Trade Taking Place /n")
-            console.log("Getting the Dai Now")
-            // We get the balance before the swap just for logging purpose
-            let ethBalanceBefore = await web3.eth.getBalance(fromWallet);
-            let daiBalanceBefore = await daiToken.methods.balanceOf(fromWallet).call();
-            await onesplitContract.methods.swap(ethAddress, daiAddress, amountWithDecimalsEth, quoteEthToDai.returnAmount, quoteEthToDai.distribution, 0x40000000).send({ from: fromWallet, gas: gasAmount, gasPrice: estimatedGasPrice, value:amountWithDecimalsEth }, async function(error, txHash) {
-                if (error) {
-                    console.log("Could not complete the swap for Dai", error);
-                    throw new Error('This is not an error in code. Error at Transaction. This is just to abort javascript');
-                    return;
-                }
-                status = await waitTransaction(txHash);
-                // We check the final balances after the swap for logging purpose
-                let ethBalanceAfter = await web3.eth.getBalance(fromWallet);
-                let daiBalanceAfter = await daiToken.methods.balanceOf(fromWallet).call();
-                console.log("Balances after getting Dai:")
-                console.log("Change in ETH balance", new BigNumber(ethBalanceAfter).minus(ethBalanceBefore).shiftedBy(-fromTokenDecimals).toFixed(2));
-                console.log("Change in DAI balance", new BigNumber(daiBalanceAfter).minus(daiBalanceBefore).shiftedBy(-fromTokenDecimals).toFixed(2));
-                if(status){
-                    console.log("Trade for Dai Succeeded.")
-                    console.log("Resuming...")
-                    daiBalanceBefore = await daiToken.methods.balanceOf(fromWallet).call();
-                    approveDaiToken(daiToken, onesplitAddress, new BigNumber(new BigNumber(daiBalanceBefore).shiftedBy(-fromTokenDecimals)).shiftedBy(fromTokenDecimals).toFixed(), estimatedGasPrice, async function() {
-                        // We get the balance before the swap just for logging purpose
-                        let ethBalanceBefore = await web3.eth.getBalance(fromWallet);
-                        await onesplitContract.methods.swap(daiAddress, ethAddress, new BigNumber(new BigNumber(daiBalanceBefore).shiftedBy(-fromTokenDecimals)).shiftedBy(fromTokenDecimals).toFixed(), quoteDaiToEth.returnAmount, quoteDaiToEth.distribution, 0x40000000).send({ from: fromWallet, gas: gasAmount, gasPrice: estimatedGasPrice }, async function(error, txHash) {
-                            if (error) {
-                                console.log("Could not complete the swap for Eth", error);
-                                return;
-                            }
-                            status = await waitTransaction(txHash);
-                            // We check the final balances after the swap for logging purpose
-                            let ethBalanceAfter = await web3.eth.getBalance(fromWallet);
-                            let daiBalanceAfter = await daiToken.methods.balanceOf(fromWallet).call();
-                            console.log("Final balances:")
-                            console.log("Change in ETH balance", new BigNumber(ethBalanceAfter).minus(ethBalanceBefore).shiftedBy(-fromTokenDecimals).toFixed(2));
-                            console.log("Change in DAI balance", new BigNumber(daiBalanceAfter).minus(daiBalanceBefore).shiftedBy(-fromTokenDecimals).toFixed(2));
-                        });
-                    });
-                throw new Error('This is not an error in code. First Successful Transaction. Check out Result');
-                } else if(!status){
-                    console.log("Trade Failed. Stopping Now")
-                    throw new Error('This is not an error in code. Error at Transaction. This is just to abort javascript');
-                }
-            });
-        }else{
-          console.log("No trade present")
+async function swappingForDai(ethAddress, daiAddress, amountWithDecimalsEth, quoteEthToDai, estimatedGasPrice) {
+    console.log("/n Swapping Trade Taking Place /n")
+    console.log("Getting the Dai Now")
+    let hash = null;
+    // We get the balance before the swap just for logging purpose
+    let ethBalanceBefore = await web3.eth.getBalance(fromWallet);
+    let daiBalanceBefore = await daiToken.methods.balanceOf(fromWallet).call();
+    await onesplitContract.methods.swap(ethAddress, daiAddress, amountWithDecimalsEth, quoteEthToDai.returnAmount, quoteEthToDai.distribution, 0x40000000).send({ from: fromWallet, gas: gasAmount, gasPrice: estimatedGasPrice, value:amountWithDecimalsEth }, async function(error, txHash) {
+        if (error) {
+            console.log("Could not complete the swap for Dai", error);
+            return;
         }
+        hash = txHash
+        // We check the final balances after the swap for logging purpose
+        let ethBalanceAfter = await web3.eth.getBalance(fromWallet);
+        let daiBalanceAfter = await daiToken.methods.balanceOf(fromWallet).call();
+        console.log("Balances after getting Dai:")
+        console.log("Change in ETH balance", new BigNumber(ethBalanceAfter).minus(ethBalanceBefore).shiftedBy(-fromTokenDecimals).toFixed(2));
+        console.log("Change in DAI balance", new BigNumber(daiBalanceAfter).minus(daiBalanceBefore).shiftedBy(-fromTokenDecimals).toFixed(2));
     });
+    return hash;
+}
+
+async function swappingForEth(ethAddress, daiAddress, quoteDaiToEth, estimatedGasPrice){
+    console.log("Trade for Dai Succeeded.")
+    console.log("Resuming...")
+    let hash = null;
+    // approveDaiToken(daiToken, onesplitAddress, new BigNumber(new BigNumber(daiBalanceBefore).shiftedBy(-fromTokenDecimals)).shiftedBy(fromTokenDecimals).toFixed(), estimatedGasPrice, async function() {
+    // Manually approve Dai to Onesplit contract on etherscan so I don't need to bother with it for a while here:  https://etherscan.io/address/0x6b175474e89094c44da98b954eedeac495271d0f#writeContract
+    // We get the balance before the swap just for logging purpose
+    daiBalanceBefore = await daiToken.methods.balanceOf(fromWallet).call();
+    let ethBalanceBefore = await web3.eth.getBalance(fromWallet);
+    await onesplitContract.methods.swap(daiAddress, ethAddress, new BigNumber(new BigNumber(daiBalanceBefore).shiftedBy(-fromTokenDecimals)).shiftedBy(fromTokenDecimals).toFixed(), quoteDaiToEth.returnAmount, quoteDaiToEth.distribution, 0x40000000).send({ from: fromWallet, gas: gasAmount, gasPrice: estimatedGasPrice }, async function(error, txHash) {
+        if (error) {
+            console.log("Could not complete the swap for Eth", error);
+            return;
+        }
+        hash = txHash
+        // We check the final balances after the swap for logging purpose
+        let ethBalanceAfter = await web3.eth.getBalance(fromWallet);
+        let daiBalanceAfter = await daiToken.methods.balanceOf(fromWallet).call();
+        console.log("Final balances:")
+        console.log("Change in ETH balance", new BigNumber(ethBalanceAfter).minus(ethBalanceBefore).shiftedBy(-fromTokenDecimals).toFixed(2));
+        console.log("Change in DAI balance", new BigNumber(daiBalanceAfter).minus(daiBalanceBefore).shiftedBy(-fromTokenDecimals).toFixed(2));
+    });
+    return hash;
+}
+
+
+
+async function Trading(daiAddress, ethAddress, amountWithDecimalsEth, estimatedGasPrice){
+    let values = await getQuotesEthToDai(ethAddress, daiAddress, amountWithDecimalsEth);
+    quoteEthToDai = values[0]
+    quoteDaiToEth = values[1]
+    if(values[2] == true){
+        console.log("Swapping Eth for Dai")
+        txHash = await swappingForDai(ethAddress, daiAddress, amountWithDecimalsEth, quoteEthToDai, estimatedGasPrice);
+        status = await waitTransaction(txHash);
+        console.log("Status of TXN Hash is: " + status)
+        if(status == true){
+            console.log("Get Eth back Now")
+            txHash = await swappingForEth(ethAddress, daiAddress, quoteDaiToEth, estimatedGasPrice)
+            status = await waitTransaction(txHash);
+            console.log("Status of TXN Hash is: " + status)
+            console.log("Just done a recent trade.")
+        }
+        return true;
+    }
+    return false;
 }
 
 let monitoringPrice = false;
 let numberOfExchanges = 0;
+let maxDiff = 0;
 
 async function monitorPrice() {
   if(monitoringPrice) {
@@ -196,8 +218,12 @@ async function monitorPrice() {
   console.log("Current Eth now: " + amountToExchange)
   amountToExchange = (amountToExchange * .65).toString()
   estimatedGasPrice = await web3.eth.getGasPrice()
-  if(Number(estimatedGasPrice) < 60000000000){
-    await Trading(daiAddress, ethAddress, amountToExchange, estimatedGasPrice)
+  if(Number(estimatedGasPrice) < 65000000000){
+    estimatedGasPrice = Number(estimatedGasPrice) + 3000000000
+    monitoringPrice = true;
+    let done = await Trading(daiAddress, ethAddress, amountToExchange, estimatedGasPrice.toString())
+    console.log("Trading Eth to Dai is Done: " + done)
+    monitoringPrice = false;
     amountToExchange = await web3.eth.getBalance(config.PUBLIC_ADDRESS);//for eth amount
     amountToExchange = (amountToExchange).toString()
     console.log("Current new balance after trading of Eth: " + amountToExchange)
@@ -208,5 +234,5 @@ async function monitorPrice() {
 
 
 // Check markets every n seconds
-const POLLING_INTERVAL = process.env.POLLING_INTERVAL || 5000 // 5 Seconds
+const POLLING_INTERVAL = process.env.POLLING_INTERVAL || 10000 // 5 Seconds
 priceMonitor = setInterval(async () => { await monitorPrice() }, POLLING_INTERVAL)
